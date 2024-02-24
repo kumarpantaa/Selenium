42987805e2995a2293e484ed283cc1f36896c98e
kumarpantaa <kumarpantaa@gmail.com>
2024-02-25 01:01:12 +0530

    Interacted With Inputs

---
+++ src/test/java/episode_01/LaunchBrowser.java
@@ -0,0 +1,46 @@
+package episode_01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		
+		String URL="https://www.youtube.com/";
+		
+		//How to launch the URL
+		driver.get(URL);
+		System.out.println("Info : --> : URL has been launched successfully");
+		
+		//How to maximize the window
+		driver.manage().window().maximize();
+		System.out.println("Info : --> : The window has been maximized successfully");
+		
+		//How to minimize the window
+		driver.manage().window().minimize();
+		System.out.println("Info : --> : The window has been minimized successfully");
+		
+		//How to get the title title
+		String title = driver.getTitle();
+		System.out.println("Info : --> : The title of the page : " + title);
+		System.out.println("Info : --> : Identified the title of the page successfully");
+		
+		//How to read the URL
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println("Info : --> : The current URL of the Page : " +currentUrl);
+		System.out.println("Info : --> : Identified the URL of the page successfully");
+		
+		//How to refresh the page
+		driver.navigate().refresh();
+		System.out.println("Info : --> : The Page has been refreshed successfully");
+		
+		//How to close the current window
+		driver.close();
+		System.out.println("Info : --> : The Window has been closed successfully");
+
+	}
+
+}
---
+++ src/test/java/episode_02/InteractInputWithLetCodePage.java
@@ -0,0 +1,59 @@
+package episode_02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteractInputWithLetCodePage {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+
+		String URL="https://letcode.in/edit";
+
+		//How to launch the URL
+		driver.get(URL);
+		System.out.println("Info : --> : URL has been launched successfully");
+
+		//Enter your full Name
+		WebElement enterYourFullNameTextBox = driver.findElement(By.cssSelector("#fullName"));
+		enterYourFullNameTextBox.sendKeys("Kumar Pantaa");
+		System.out.println("Info : --> : Entered Fullname successfully");
+
+		//Append a text and press keyboard tab
+		WebElement appendTextBox = driver.findElement(By.id("join"));
+		appendTextBox.sendKeys(" in automation" + Keys.TAB);
+		System.out.println("Info : --> : Appended the text and pressed the tab button in the keyboard successfully");
+
+		//What is inside the text box
+		WebElement insideTheTextBox = driver.findElement(By.xpath("//input[@id='getMe']"));
+		String insideTextOfTheBox = insideTheTextBox.getText();
+		System.out.println("Info : --> : Inside text of the box is : " + insideTextOfTheBox);
+		System.out.println("Info : --> : Identified the text in the box successfully");
+
+		//Clear the text
+		WebElement clearTheText = driver.findElement(By.id("clearMe"));
+		clearTheText.clear();
+		System.out.println("Info : --> : Cleared the text in the box successfully");
+
+		//Confirm edit field is disabled
+		WebElement disabledTextBox = driver.findElement(By.id("noEdit"));
+		boolean disabled = disabledTextBox.isEnabled();
+		System.out.println("Info : --> : Is the textbox is enabled ? : " + disabled);
+		System.out.println("Info : --> : Verifed the textBox is disabled successfully");
+
+		//Confirm text is read-only
+		WebElement readOnlyTextBox = driver.findElement(By.id("dontwrite"));
+		boolean readOnly = readOnlyTextBox.getAttribute("readonly").equals("true");
+		System.out.println("Info : --> : Is this readonly ? : " + readOnly);
+
+		//How to close the current window
+		driver.close();
+		System.out.println("Info : --> : The Window has been closed successfully");
+
+	}
+
+}
